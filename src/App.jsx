fe95fe1b7a4600af435ac6599f24753139f83eb7
thekingsleyhemans <kingsleyhemans@gmail.com>
2026-05-11 18:05:17 +0000

    refactor: move HeaderNav to App component and remove from main and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import Archives from "./pages/Archives";
 import ProjectDetails from "./pages/ProjectDetails";
+import HeaderNav from "./components/ui/HeaderNav";
 
 export default function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/archives" element={<Archives />} />
-      <Route path="/project/:slug" element={<ProjectDetails />} />
-    </Routes>
+    <>
+      {location.pathname !== '/archives' && <HeaderNav />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/archives" element={<Archives />} />
+        <Route path="/project/:slug" element={<ProjectDetails />} />
+      </Routes>
+    </>
   );
 }
